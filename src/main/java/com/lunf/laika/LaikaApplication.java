de4c16d8fb084833a007d7567dfea881ef85ca37
lunf <nhatcuong.nguyen@gmail.com>
2018-06-13 14:59:08 +0700

    First commit

---
+++ src/main/java/com/lunf/laika/LaikaApplication.java
@@ -0,0 +1,86 @@
+package com.lunf.laika;
+
+import com.lunf.laika.Utils.Utils;
+import com.lunf.laika.core.Bin;
+import com.lunf.laika.core.BinPacking;
+import com.lunf.laika.primitives.MArea;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class LaikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaikaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's try to resolve the solution");
+
+			launch("Rectangles.txt");
+
+		};
+	}
+
+	private void launch(String fileName) throws IOException {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream in = classLoader.getResourceAsStream(fileName);
+		if (in == null) {
+			throw new IOException("Could not read file");
+		}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		Object[] result = Utils.loadPieces(reader);
+
+		Dimension binDimension = (Dimension) result[0];
+		Dimension viewPortDimension = (Dimension) result[1];
+		MArea[] pieces = (MArea[]) result[2];
+
+		Bin[] bins = BinPacking.BinPackingStrategy(pieces, binDimension, viewPortDimension);
+		System.out.println("Generating bin images.........................");
+		drawbinToFile(bins, viewPortDimension);
+		System.out.println();
+		System.out.println("Generating bin description files....................");
+		createOutputFiles(bins);
+		System.out.println("DONE!!!");
+
+	}
+
+	private void drawbinToFile(Bin[] bins, Dimension viewPortDimension) throws IOException {
+		for (int i = 0; i < bins.length; i++) {
+
+			MArea[] areasInThisbin = bins[i].getPlacedPieces();
+			ArrayList<MArea> areas = new ArrayList<MArea>();
+			for (MArea area : areasInThisbin) {
+				areas.add(area);
+			}
+			Utils.drawMAreasToFile(areas, viewPortDimension, bins[i].getDimension(), ("results/Bin-" + String.valueOf(i + 1)));
+			System.out.println("Generated image for bin " + String.valueOf(i + 1));
+		}
+	}
+
+	private void createOutputFiles(Bin[] bins) throws IOException {
+		for (int i = 0; i < bins.length; i++) {
+			PrintWriter writer = new PrintWriter("results/Bin-" + String.valueOf(i + 1) + ".txt", "UTF-8");
+			writer.println(bins[i].getPlacedPieces().length);
+			MArea[] areasInThisbin = bins[i].getPlacedPieces();
+			for (MArea area : areasInThisbin) {
+				double offsetX = area.getBoundingBox2D().getX();
+				double offsetY = area.getBoundingBox2D().getY();
+				writer.println(area.getID() + " " + area.getRotation() + " " + offsetX + "," + offsetY);
+			}
+			writer.close();
+			System.out.println("Generated points file for bin " + String.valueOf(i + 1));
+		}
+	}
+}
---
+++ src/main/java/com/lunf/laika/core/Constants.java
@@ -0,0 +1,43 @@
+package com.lunf.laika.core;
+
+/**
+ * Constants variables used along the application.
+ *
+ * @author Moises Baly
+ */
+public class Constants {
+    /**
+     * Division factor when trying different positions to drop the piece along
+     * the X axes. Increasing this factor will produce smaller horizontal steps.
+     */
+    public static final int DIVE_HORIZONTAL_DISPLACEMENT_FACTOR = 3;
+    /**
+     * Division factor for the displacement of pieces when trying to Increasing
+     * this factor will produce smaller horizontal steps.
+     */
+    public static final int DX_SWEEP_FACTOR = 10;
+    /**
+     * Division factor for the displacement of pieces when trying to place
+     * pieces inside others in the maximal rectangles optimization step.
+     * Increasing this factor will produce smaller vertical steps.
+     */
+    public static final int DY_SWEEP_FACTOR = 2;
+    /**
+     * This is the vector of angles for which we will try to drop a piece in the
+     * simulation algorithm. Adding angles to the vector will result in that we
+     * will try them until a valid position is found or until all options are
+     * exhausted.
+     */
+    public static final int[] ROTATION_ANGLES = {0, 90};
+
+    /**
+     * This is standard board width size in millimeters
+     */
+    public static final int STANDARD_MAX_WIDTH = 1200;
+
+    /**
+     * This is standard board height in millimeters
+     */
+    public static final int STANDARD_MAX_HEIGHT = 2400;
+
+}
